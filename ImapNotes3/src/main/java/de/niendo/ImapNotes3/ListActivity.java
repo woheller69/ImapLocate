08722705bdd52c51d63015326d4f79fbc067a143
niendo1 <peter@niendo.de>
2022-12-17 22:07:35 +0100

    rework icon
    Signed-off-by: niendo1 <peter@niendo.de>

--- ImapNotes3/src/main/java/de/niendo/ImapNotes3/ListActivity.java
+++ ImapNotes3/src/main/java/de/niendo/ImapNotes3/ListActivity.java
@@ -469,7 +469,7 @@ public class ListActivity extends AppCompatActivity implements OnItemSelectedLis
                 about += getString(R.string.internet) + "\n";
                 new AlertDialog.Builder(this)
                         .setTitle(getString(R.string.about) + " " + BuildConfig.APPLICATION_NAME)
-                        .setIcon(R.drawable.imapnotes)
+                        .setIcon(R.drawable.ic_launcher)
                         .setMessage(about)
                         .setPositiveButton("OK", (dialog, which) -> {
                             // Do nothing
